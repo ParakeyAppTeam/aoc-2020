68723f6220296a96e6e6ef3484ccf373fab84a10
App Team <appteam@parakey.co>
2025-02-14 13:08:58 +0100

    day 1 part 2

--- day1/part_two.go
+++ day1/part_two.go
@@ -1,3 +1,22 @@
 package main
 
-func part_two(input string) {}
+import "fmt"
+
+func goSum3(list []int) int {
+	for i, value1 := range list {
+		for j, value2 := range list[i:] {
+			for _, value3 := range list[j:] {
+				if value1+value2+value3 == 2020 {
+					return value1 * value2 * value3
+				}
+			}
+		}
+	}
+	panic("Crash boom bang")
+}
+
+func part_two(input string) {
+	list := parseInput(input)
+	result := goSum3(list)
+	fmt.Println(result)
+}
