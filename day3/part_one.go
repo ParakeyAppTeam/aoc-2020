f1785d8fcbe87deac93d2c6ecad88c242d856b50
App Team <appteam@parakey.co>
2025-03-07 13:51:37 +0100

    day 3 part 1

--- day3/part_one.go
+++ day3/part_one.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func part_one(input string) {
-	fmt.Println(input)
+	fmt.Println(solve_one(parse(input), 3, 1))
+}
+
+func parse(input string) (out [][]string) {
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		out = append(out, strings.Split(line, ""))
+	}
+	return out
+}
+
+func solve_one(slope [][]string, right int, down int) (out int) {
+	x, y := 0, 0
+
+	for {
+		if y >= len(slope) {
+			break
+		}
+
+		if slope[y][x] == "#" {
+			out++
+		}
+
+		x += right
+		y += down
+
+		x = x % len(slope[0])
+	}
+
+	return out
 }
--- day3/part_one_test.go
+++ day3/part_one_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestPasswordValidation(t *testing.T) {
+	input := `.#.
+#..
+.##`
 
+	output := parse(input)
+
+	expect := [][]string{
+		{".", "#", "."},
+		{"#", ".", "."},
+		{".", "#", "#"},
+	}
+
+	if !reflect.DeepEqual(expect, output) {
+		t.Errorf("Error test result! result: %v", output)
+	}
+
+}
+
+func TestTreeCount(t *testing.T) {
+	input := [][]string{
+		{".", "#", "."},
+		{"#", ".", "."},
+		{".", "#", "#"},
+	}
+
+	output := solve_one(input, 1, 1)
+
+	if output != 1 {
+		t.Errorf("Error test result! result: %v", output)
+	}
+}
+
+func TestWrapAround(t *testing.T) {
+	input := [][]string{
+		{".", "#", "."},
+		{"#", ".", "#"},
+		{".", "#", "#"},
+	}
+
+	output := solve_one(input, 2, 1)
+
+	if output != 2 {
+		t.Errorf("Error test result! result: %v", output)
+	}
 }
